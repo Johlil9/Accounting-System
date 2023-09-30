ae2b59b66a6c2330ecbc367775cc50bc96bf2eeb
Johlil9 <243333@usn.no>
2023-09-30 02:38:08 +0200

    Display for main information added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,14 @@ import Navigation from './components/navigation/Navigation'
 import { MainLayout } from './styles/Layout'
 function App() {
   const [active, setActive] = useState(1)
+ 
   return (
     <AppStyled className = "App">
       <MainLayout>
         <Navigation active = {active} setActive = {setActive} />
+        <main>
+         
+        </main>
       </MainLayout>
     </AppStyled>
   );
@@ -16,6 +20,18 @@ function App() {
 const AppStyled = styled.div`
   height: 100vh;
   background-color: gray;
+  position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
 `;
 
 export default App;
