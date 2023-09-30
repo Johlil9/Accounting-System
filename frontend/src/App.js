d6377fba89077f7ee2ae580b82ded343e7f93362
Johlil9 <243333@usn.no>
2023-09-30 03:08:13 +0200

    Pages imported and works.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,37 @@
 import React, { useState } from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 import Navigation from './components/navigation/Navigation'
 import { MainLayout } from './styles/Layout'
+
+// Pages
+import Dashboard from './pages/Dashboard'
+import Income from './pages/Income'
+import Expenses from './pages/Expenses'
+
 function App() {
   const [active, setActive] = useState(1)
   
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4: 
+        return <Expenses />
+      default: 
+        return <Dashboard />
+    }
+  }
+
   return (
     <AppStyled className = "App">
       <MainLayout>
         <Navigation active = {active} setActive = {setActive} />
         <main>
-          
+          {displayData}
         </main>
       </MainLayout>
     </AppStyled>
