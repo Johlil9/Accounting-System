8ddeac51b9950855009b88c901cfcabf993107f0
Johlil9 <243333@usn.no>
2023-10-06 02:25:23 +0200

    View over total income, expense and balance

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../styles/Layout'
 import Chart from '../components/chart/Chart'
+import { useGlobalContext } from '../context/GlobalContext'
 function Dashboard() {
+    const {totalIncome, totalBalance, totalExpenses, getIncome, getExpenses} = useGlobalContext()
+
+    useEffect(() => {
+        getIncome()
+        getExpenses()
+    }, [])
+
     return (
         <DashboardStyled>
             <InnerLayout>
-                <h1>Dashboard</h1>
-                <Chart />
+            <h1>All Transactions</h1>
+                <div className="stats-con">
+                    <div className="chart-con">
+                        <Chart />
+                        <div className="amount-con">
+                            <div className="income">
+                                <h2>Total Income</h2>
+                                <p>
+                                    {totalIncome()}
+                                </p>
+                            </div>
+                            <div className="expense">
+                                <h2>Total Expense</h2>
+                                <p>
+                                    {totalExpenses()}
+                                </p>
+                            </div>
+                            <div className="balance">
+                                <h2>Total Balance</h2>
+                                <p>
+                                    {totalBalance()}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="history-con">
+                    </div>
+                </div>
             </InnerLayout>
         </DashboardStyled>
     )
